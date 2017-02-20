a3a654fd5f375cfde184b8cb516bb31a3844b4af
Dominique Northecide <domnorth@gmail.com>
2017-02-20 10:15:52 -0500

    update server.js

--- containerengine/hello-world/server.js
+++ containerengine/hello-world/server.js
@@ -17,7 +17,7 @@
 var http = require('http');
 var handleRequest = function (req, res) {
   res.writeHead(200);
-  res.end('Hello World v19.2 from Travis & gCloud Kubernetes! Mon Feb 20 80:59:08 EST 2017');
+  res.end('Hello World v23.1 from Travis & gCloud Kubernetes! Mon Feb 20 77:77:77 EST 2017');
 
 };
 var www = http.createServer(handleRequest);
